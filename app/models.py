1ba9c079019d1ecb1e4cf5f094e338aa74ad90f6
Micheal Waldrup <waldrupm@gmail.com>
2020-06-30 16:27:44 -0400

    do stuff

--- app/models.py
+++ app/models.py
@@ -1,22 +1,64 @@
-from . import db, login
-from werkzeug.security import generate_password_hash, check_password_hash
+from . import db, login_manager
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+followers = db.Table(
+    "followers",
+    db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("user.id")),
+)
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-
-    def generate_password(self, original_password):
-        self.password = generate_password_hash(original_password)
-
-    def check_password(self, original_password):
-        return check_password_hash(self.password, original_password)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+    posts = db.relationship("Post", backref="posts", lazy="dynamic")
+    followed = db.relationship(
+        "User",
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"),
+        lazy="dynamic",
+    )
 
     def __repr__(self):
-        return '<User {}>'.format(self.name)
+        return f"User: {self.name}"
 
-@login.user_loader
+    def generate_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, pw_to_check):
+        return check_password_hash(self.password, pw_to_check)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+            ).count() > 0
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    body = db.Column(db.Text)
+    created_on = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f"<Post: {self.body[:30]}...>"
+
+
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
--- app/routes.py
+++ app/routes.py
@@ -1,55 +1,113 @@
-from . import app, db, login
+from app import app, db, login_manager
+from app.models import User, Post
 from flask import render_template, request, redirect, url_for, flash
-from .models import User
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        posts = Post.query.order_by(Post.created_on).all()
+        return render_template("index.html", posts=posts)
+    elif request.method == "POST":
+        post_body = request.form.get("post_body")
+        p = Post(body=post_body, user_id=current_user.id)
+        db.session.add(p)
+        db.session.commit()
+        flash("Post created.", "success")
+        return redirect(url_for("index"))
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        remember_me = request.form.get('remember_me')
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        remember_me = request.form.get("remember_me")
 
         user = User.query.filter_by(email=email).first()
-        if user is None or not user.check_password(password):
-            flash('Either your email address or password is incorrect. Try again.', 'warning')
-            return redirect(url_for('login'))
-        login_user(user, remember=remember_me)
-        flash('User logged in successfully.', 'info')
-        return redirect(url_for('home'))
-    return render_template('login.html')
+        if user:
+            if user.check_password(password):
+                login_user(user, remember=remember_me)
+                flash("Login accepted", "success")
+                return redirect(url_for("index"))
+        flash("Invalid user / pass combination", "warning")
+        return render_template("login.html")
 
-@app.route('/register', methods=['GET', 'POST'])
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        confirm_password = request.form.get('confirm_password')
+    if request.method == "POST":
+        name = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
 
-        user = User.query.filter_by(email=email).first()
-        if user is not None:
-            flash('Email already exists. Use another one.', 'danger')
-            return redirect(url_for('register'))
-        if confirm_password != password:
-            flash("Your passwords don't match. Try again.", 'warning')
-            return redirect(url_for('register'))
-        else:
-            u = User(name=name, email=email, password=password)
-            u.generate_password(u.password)
+        if User.query.filter_by(email=email).first():
+            flash("That email is already in use.", "danger")
+            return redirect(url_for("register"))
 
-            db.session.add(u)
-            db.session.commit()
-            flash('User registered successfully.', 'success')
-            return redirect(url_for('login'))
-    return render_template('register.html')
+        if password != confirm_password:
+            flash("Password and confirmation must be the same.", "danger")
+            return redirect(url_for("register"))
+        u = User(name=name, email=email, password=password)
+        u.generate_password(u.password)
 
-@app.route('/logout')
+        db.session.add(u)
+        db.session.commit()
+        flash("User registration successful.", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+
+@app.route("/logout", methods=["GET"])
 def logout():
     logout_user()
-    flash('You have been logged out successfully.', 'info')
-    return redirect(url_for('login'))
+    flash("Logged out successfully", "info")
+    return redirect(url_for("login"))
+
+
+@app.route("/profile")
+def profile():
+    posts = User.query.get(current_user.id).posts.all()
+    return render_template("profile.html", posts=posts)
+
+
+@app.route("/profile/post/<int:id>")
+def profile_single(id):
+    p = Post.query.get(id)
+    post = p
+    return render_template("profile-single.html", post=post)
+
+
+@app.route("/post/<int:id>")
+def index_single(id):
+    p = Post.query.get(id)
+    post = p
+    return render_template("index-single.html", post=post)
+
+
+@app.route("/users")
+def users():
+    users = [i for i in User.query.all() if i.id != current_user.id]
+    return render_template("users.html", users=users)
+
+
+@app.route("/users/follow/<int:id>")
+def user_follow(id):
+    user = User.query.get(id)
+    current_user.follow(user)
+    db.session.commit()
+    flash(f"You have followed {user.name}", "success")
+    return redirect(url_for("users"))
+
+
+@app.route("/users/unfollow/<int:id>")
+def user_unfollow(id):
+    user = User.query.get(id)
+    current_user.unfollow(user)
+    db.session.commit()
+    flash(f"You have unfollowed {user.name}", "success")
+    return redirect(url_for("users"))

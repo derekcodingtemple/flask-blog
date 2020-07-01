9c57725a9ac9d001341e6db5a535e87670e857c2
derekcodingtemple <derekh@codingtemple.com>
2020-07-01 15:48:43 -0500

    added more stuff

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,35 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 
-app = Flask(__name__)
-
-# Setup config object
-
-app.config.from_object(Config)
-
-# Setup database and ORM
-
-db = SQLAlchemy(app)
-
-# Add db migrations
-
-migrate = Migrate(app, db)
-
-
+db = SQLAlchemy()
+migrate = Migrate()
 login_manager = LoginManager()
-login_manager.init_app(app)
+moment = Moment()
 
-from app import routes, models
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+    login_manager.login_message_category = 'warning'
+
+    moment.init_app(app)
+
+    from app.blueprints.account import account
+    app.register_blueprint(account, url_prefix='/account')
+
+    from app.blueprints.users import users
+    app.register_blueprint(users, url_prefix='/users')
+
+    with app.app_context():
+        from app import routes
+
+    return app
+
+from app import models
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,17 @@
-from app import app, db, login_manager
+from app import db, login_manager
 from app.models import User, Post
-from flask import render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, current_user
+from flask import render_template, request, redirect, url_for, flash, current_app as app
+from flask_login import login_user, logout_user, current_user, login_required
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
+    if current_user.is_authenticated:
+        posts = current_user.followed_posts()
+    else:
         posts = Post.query.order_by(Post.created_on).all()
-        return render_template("index.html", posts=posts)
-    elif request.method == "POST":
+    
+    if request.method == "POST":
         post_body = request.form.get("post_body")
         p = Post(body=post_body, user_id=current_user.id)
         db.session.add(p)
@@ -17,59 +19,11 @@ def index():
         flash("Post created.", "success")
         return redirect(url_for("index"))
 
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password")
-        remember_me = request.form.get("remember_me")
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            if user.check_password(password):
-                login_user(user, remember=remember_me)
-                flash("Login accepted", "success")
-                return redirect(url_for("index"))
-        flash("Invalid user / pass combination", "warning")
-        return render_template("login.html")
-
-    return render_template("login.html")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        name = request.form.get("username")
-        email = request.form.get("email")
-        password = request.form.get("password")
-        confirm_password = request.form.get("confirm_password")
-
-        if User.query.filter_by(email=email).first():
-            flash("That email is already in use.", "danger")
-            return redirect(url_for("register"))
-
-        if password != confirm_password:
-            flash("Password and confirmation must be the same.", "danger")
-            return redirect(url_for("register"))
-        u = User(name=name, email=email, password=password)
-        u.generate_password(u.password)
-
-        db.session.add(u)
-        db.session.commit()
-        flash("User registration successful.", "success")
-        return redirect(url_for("login"))
-    return render_template("register.html")
-
-
-@app.route("/logout", methods=["GET"])
-def logout():
-    logout_user()
-    flash("Logged out successfully", "info")
-    return redirect(url_for("login"))
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/profile")
+@login_required
 def profile():
     posts = User.query.get(current_user.id).posts.all()
     return render_template("profile.html", posts=posts)
@@ -84,30 +38,5 @@ def profile_single(id):
 
 @app.route("/post/<int:id>")
 def index_single(id):
-    p = Post.query.get(id)
-    post = p
+    post = Post.query.get(id)
     return render_template("index-single.html", post=post)
-
-
-@app.route("/users")
-def users():
-    users = [i for i in User.query.all() if i.id != current_user.id]
-    return render_template("users.html", users=users)
-
-
-@app.route("/users/follow/<int:id>")
-def user_follow(id):
-    user = User.query.get(id)
-    current_user.follow(user)
-    db.session.commit()
-    flash(f"You have followed {user.name}", "success")
-    return redirect(url_for("users"))
-
-
-@app.route("/users/unfollow/<int:id>")
-def user_unfollow(id):
-    user = User.query.get(id)
-    current_user.unfollow(user)
-    db.session.commit()
-    flash(f"You have unfollowed {user.name}", "success")
-    return redirect(url_for("users"))
